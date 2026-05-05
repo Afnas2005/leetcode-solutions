27af66a398271f9663110401ac7b05dfe1069547
Afnas2005 <afnasappu2005@gmail.com>
2026-05-05 11:56:27 +0530

    Time: 0 ms (100%), Space: 55.1 MB (7.05%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+function getRow(rowIndex) {
+    let row = [1];
+
+    for (let i = 1; i <= rowIndex; i++) {
+        row[i] = (row[i - 1] * (rowIndex - i + 1)) / i;
+    }
+
+    return row;
+}
